abbf9d98868008f29152b7747545cac32d6a8856
AndiMahardika <andi.mahardika.mulya@gmail.com>
2024-12-01 18:09:26 +0700

    fix: error register

--- src/features/auth/service/api.register.js
+++ src/features/auth/service/api.register.js
@@ -1,8 +1,8 @@
 import instance from "../../../utils/instance.js";
 
-export async function apiRegister({nama_lengkap, tanggal_lahir, no_telepon, email, password}) {
+export async function apiRegister({nama_lengkap,  email, tanggal_lahir, no_telepon, password}) {
  try {
-   const response = await instance.post('/register', {nama_lengkap, tanggal_lahir, no_telepon, email, password})
+   const response = await instance.post('/register', {nama_lengkap, email, tanggal_lahir, no_telepon, password})
    return response.data;
  } catch (error) {
    throw new Error("Error registering user.");
