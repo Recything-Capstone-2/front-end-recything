88825e5c739be64af7c729a01898f7961d2481d9
Widarft <adibseptiawan@gmail.com>
2024-12-09 20:35:02 +0700

    update about us mobile responsive

--- src/features/about-us/components/VisionSection.jsx
+++ src/features/about-us/components/VisionSection.jsx
@@ -22,7 +22,7 @@ const VisionSection = () => {
           <img
             src="https://s3-alpha-sig.figma.com/img/6930/9f18/b896e805a5bb3febd54339d45dbc7617?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=f9-7GC8xiDcZfm~7hdHMx~P0lGMo8u8ipx0z53n3eZWN1G25OlCPtZI0A2H61hFk2sCXp9hts44LUwByb1vpms-O9hZcV5HqYLoSfHCvo~Z-jw1tnS4vBRtJo5C~8YJ-YvvN36HX~6Rj5UQ61e6JgA-VL9F0et88FPuAgFdGlf0CteVrQaq0R~PBWRH8~jNdwazqBhXK~c~LMO6WZXM-Teod1mE2w6WXOevtGrHRpIiqIwgOGiU4ZkuNBy6eAE1wkx0xFvAwTShpkbAjrJ63ScQFmI5CZaUtZUdLY3Iki1~xe8AoMe2w3vSiA2mBSuowtoKVGOEQfmDJ~KIndqxnKg__"
             alt="Foto Visi"
-            className="rounded-lg shadow-lg object-cover w-full h-100"
+            className="rounded-lg shadow-lg object-cover w-full"
           />
         </div>
       </div>
