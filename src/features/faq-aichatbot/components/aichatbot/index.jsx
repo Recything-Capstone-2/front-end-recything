ff856b2a948f1d973cb70a1df10f2dcbfbeac286
Widarft <adibseptiawan@gmail.com>
2024-12-14 23:19:15 +0700

    bugfix minor feature

--- src/features/faq-aichatbot/components/aichatbot/index.jsx
+++ src/features/faq-aichatbot/components/aichatbot/index.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import ChatDrawer from "./ChatDrawer";
 import AiChatbotSection from "./AiChatbotSection";
-import { history } from "../../constant/history";
 
 const AiChatbotPage = () => {
   return (
     <div className="flex max-w-[1440px] bg-green-50 dark:bg-gray-800">
-      {/* <ChatDrawer history={history} /> */}
       <AiChatbotSection />
     </div>
   );
